c28a610228e6435d90d14f024beb5fc6e8819680
Jerry Deligiannis <deligiannisjerry@gmail.com>
2024-01-05 19:44:07 +0200

    Add posters with additional fixes

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def get_external_ip():
     if NEO4J_IP == 'localhost':
         return 'localhost'
     else:
-        shutil.copyfile('../external_config.py', 'external_config.py')
+        shutil.copyfile('../external_config.py', 'repo/external_config.py')
         try:
             from external_config import EXTERNAL_IP
 
